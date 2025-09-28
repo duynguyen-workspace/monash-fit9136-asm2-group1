3bdcd9e38d9d61798f17429d885e8a009b400aa2
Nguyen Anh Duy (AJ) <duynguyen.workspace@gmail.com>
2025-09-28 12:53:21 +1000

    Update Task4 code in Task6.py

--- set2/task6/task4.py
+++ set2/task6/task4.py
@@ -1,4 +1,3 @@
-# copy your task4 code here
 from typing import Tuple, List, Set, Optional
 import os
 
@@ -6,12 +5,15 @@ def get_stopwords(stopwords_file: str) -> List[str]:
     """
     This function gets all the stop words from the file 
 
-    Params:
-        1. stopwords_file: <str> the file name containing the stop words
+    Args:
+        1. stopwords_file (str): the file name containing the stop words
 
     Return:
-        stopwords: <list> a list of strings (the stop words)
+        stopwords (List[str]): a list of strings (the stop words)
     """
+    if not stopwords_file:
+        return []
+    
     stopwords = []
 
     with open(stopwords_file, "r") as f:
@@ -23,94 +25,96 @@ def get_stopwords(stopwords_file: str) -> List[str]:
 
 def get_vocabs(text: str, stopwords: List) -> Tuple[Tuple[str], Tuple[int]]:
     """
-    This function splits the text into words and count number of time each word appears
+    This function splits the text into words and 
+    count number of time each word appears
     
-    Params:
-        1. text: <str> the unformatted text string
-        2. stopwords: <str> the word to be filtered out
+    Args:
+        1. text (str): the unformatted text string
+        2. stopwords (str): the word to be filtered out
     
     Returns:
-        result: <tuple> A tuple that contains 2 tuples
+        vocabs (Tuple[Tuple[str], Tuple[int]]): A tuple that contains 2 tuples
         - the_word_list: list of words in the text string
         - the_count_list: number of iteration that each word appears
     """
-    if text is None or len(text) == 0:
+    if not text:
         return ()
 
-    # CONSTANTS
-    PUNCTUATIONS = "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
-
-    # PROCESS: process the word list and generate a word dictionary containing its count
-    words = get_words(text, PUNCTUATIONS)
+    # process the text, extract the word list and generate a dictionary containing word count
+    words = get_words(text)
     words_dict = {} 
 
     for word in words:
-        # SUBPROCESS: skip the word if word length < 2, word contains number or word is a stopword
-        word_size = len(word)
-        formatted_word = word.lower()
-
-        if word_size < 2 or formatted_word in stopwords or check_word_has_number(formatted_word):
+        # Skip if word is a stopword
+        if word in stopwords:
             continue
 
-        # SUBPROCESS: add / update word (lowercase) into dictionary
-        if formatted_word not in words_dict.keys():
-            words_dict[formatted_word] = 1
+        # add / update word into dictionary
+        if word not in words_dict.keys():
+            words_dict[word] = 1
         else:
-            words_dict[formatted_word] += 1
+            words_dict[word] += 1
 
     # --- MAIN OUTPUT: return the result tuple (if the dictionary is not empty)
-    if len(words_dict) == 0:
+    if not words_dict:
         return ()
 
-    # SUBPROCESS - return all the unique words and the word count in sorted order
+    # sorted all the words in ascending order
     the_word_lst = list(words_dict.keys())
-    the_word_lst.sort() # sorted all the words
+    the_word_lst.sort() 
 
-    if the_word_lst[0] is None: # remove the first item (which is an empty string '')
-        the_word_lst.pop(0) 
-
-    the_count_lst = []
-    for word in the_word_lst:
-        the_count_lst.append(words_dict[word])
-
-    # Type casting from List -> Tuple
+    # return two tuples of unique words and the word's count in sorted order
+    the_count_lst = [words_dict.get(word) for word in the_word_lst]
     the_word_lst = tuple(the_word_lst)
     the_count_lst = tuple(the_count_lst)
-    result = (the_word_lst, the_count_lst)
+    
+    vocabs = (the_word_lst, the_count_lst)
 
-    return result
+    return vocabs
 
-def get_words(text: str, delimiters: str) -> list:
+def get_words(text: str) -> List[str]:
     """
-    This function extract a list of (lowercase) words from the input text
+    This function extract a list of (lowercase) cleaned words from the input text
+    based on the given requirements
 
-    Params:
-        1. text: <str> the input text string
-        2. delimiters: <str> the symbols / characters that should be filtered from the word
+    Args:
+        1. text (str): the input text string
     
     Returns:
-        words: <list> a list of lowercase words
-    """
-    # PROCESS: iterate each character, form into a word and add to word list
-    words = []
-    word_buffer = ""
+        words (List[str]): a list of lowercase words
 
+    Requirements:
+        1. all words have been converted to lowercase
+        2. clean all the punctuations and contractions
+        3. filtering out numbers and words composed entirely of digits
+        4. discarding words with a length less than 2
+    """
+    # CONSTANTS
+    PUNTUATIONS = "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+    
+    # iterate each character, form into a word and add to word list
+    words = []
+    
+    word_buffer = ""
     for char in text:
-        # SUBPROCESS: while buffer is empty, skip the character if it is a blank space or a delimeter
-        if len(word_buffer) == 0 and (char.isspace() or char in delimiters):
+        # While buffer is empty -> skip the character if it is a blank space / a delimeter
+        if not word_buffer and (char.isspace() or char in PUNTUATIONS):
             continue
 
-        # SUBPROCESS: add character to buffer and extract word to the list 
-        if char not in delimiters and not char.isspace():
+        # Add character to buffer, extract (lowercase) word to list, and reset buffer
+        if char not in PUNTUATIONS and not char.isspace(): 
             word_buffer += char
         else:
-            # SUBPROCESS: add character to buffer, extract word to the list and reset buffer
+            # check if buffer has a valid length and contain no number
             word = word_buffer.lower()
-            words.append(word)
-            word_buffer = ""
-    
-    # SUBPROCESS: Add the remaining buffer (last word) to list 
-    if word_buffer:
+
+            if len(word) >= 2 and not check_word_has_number(word):
+                words.append(word)
+                
+            word_buffer = "" # reset buffer
+        
+    # Add the remaining buffer (last word) to list (must satisfy requirements)
+    if word_buffer and len(word_buffer) >= 2 and not check_word_has_number(word_buffer):
         words.append(word_buffer.lower())
 
     return words
@@ -119,12 +123,11 @@ def check_word_has_number(word: str) -> bool:
     """
     This function check if a word contain a number (digit)
 
-    Params:
-        1. word: <str> the word to be checked
+    Args:
+        1. word (str): the word to be checked
     
     Returns:
-        - <bool>: a boolean check if that function has a number or not
-
+        bool: a boolean check if that function has a number or not
     """
     for char in word:
         if char.isdigit():
@@ -140,23 +143,25 @@ def process_mini_dataset(
     """
     This function read all the .txt files from the data path and generate a vocabulary list 
 
-    Params:
-        1. stop_words: <Set[str]> a list of word that should not be included in the vocab list
-        2. data_path: <str> the path to the 'data' folder that contains the .txt files 
-        3. category: <str> the category name 
+    Args:
+        1. stop_words (Set[str]): a list of word that should not be included in the vocab list
+        2. data_path (str): the path to the 'data' folder that contains the .txt files 
+        3. category (str): the category name 
     
     Return:
-        - vocabs: <((str), (int))> a list of vocabulary consists of two tuples:
+        vocabs (Tuple[Tuple[str], Tuple[int]]): a tuple of vocabulary consists of two tuples:
             + (str) the list of vocabularies name
             + (int) the count of each vocabulary appears
+            (same return type as get_vocabs function)
 
     Notes:
-        - if no category --> generate vocabularies from all text files (accross all categories)
-        - if a category is provided --> generate vocabullaries from all the text file in that category folder
-
+        1. if no category 
+        --> generate vocabularies from all text files (accross all categories)
+        2. if a category is provided 
+        --> generate vocabullaries from all the text file in that category folder
     """
 
-    # PROCESS: get all the files / folders in the data path
+    # get all the files / folders in the data path
     all_files_in_dir = os.listdir(data_path)
     curr_data_path = data_path
 
@@ -168,13 +173,13 @@ def process_mini_dataset(
     
     # @CASE 2: a category is provided --> get vocabs from all .txt file of that category       
     else:
-        # SUBPROCESS: get all the category folders
+        # get all the category folders (by checking non .txt file / folder)
         categories = []
         for file_name in all_files_in_dir:
             if not file_name.endswith(".txt"):
                 categories.append(file_name)
 
-        # SUBPROCESS: concatenate all the .txt file from each category folder 
+        # concatenate all the .txt file from each category folder 
         text = ""
         for category_name in categories:
             category_path = os.path.join(curr_data_path, category_name)
@@ -187,20 +192,23 @@ def process_mini_dataset(
 
 def get_text_from_files(data_path: str) -> str:
     """
-    This function read all the .txt file inside the path and concatenate its content into 
-    a string
+    This function read all the .txt file inside the path and 
+    concatenate its content into a string
 
-    Params:
-        1. data_path: <str> the path to the folder containing the files
+    Args:
+        1. data_path (str): the path to the folder containing the files
     
     Returns:
-        - text: <str> the content of all files in that folder
+        text (str): the content of all files in that folder
     """
+    # Get all the files inside the directory
     all_files_in_dir = os.listdir(data_path)
 
+    # Extract the text from each file and concatenate all of the content together
     text = ""
     for file_name in all_files_in_dir:
         file_path = os.path.join(data_path, file_name)
+        
         with open(file_path, "r") as f:
             content = f.read().strip()
             text += f"{content} "
@@ -213,7 +221,7 @@ def get_text_from_files(data_path: str) -> str:
 if __name__ == "__main__":
     # your testing code goes here
     stopwords = get_stopwords("data/stop_words_english.txt")
-
+    # print(get_words("a b c 123 !@# apple ap pl e"))
     # print(get_vocabs("BUL, Afghanistan - Government troops intervened in Afghanistan's latest outbreak of deadly fighting between warlords, flying from the capital to the far west on U.S. and NATO airplanes to retake an air base contested in the violence, officials said Sunday...", stopwords))
     process_mini_dataset(stopwords, 'data', "Business")
     # process_mini_dataset(stopwords, 'data')
